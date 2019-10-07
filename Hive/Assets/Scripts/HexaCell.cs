79171986833e0cfafb2ab80baf030c87e1d527e8
talioo1997 <talioo1997@gmail.com>
2019-10-07 12:08:28 +0300

    Add game field. Closes #2

---
+++ Hive/Assets/Scripts/HexaCell.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class HexaCell : MonoBehaviour
+{
+    [SerializeField] SOHexaInfo hexaInfo;
+    [SerializeField] CellParams cellParams;
+
+    private void Start()
+    {
+        if (!cellParams.IsEmpty) hexaInfo.cellsOnScene.Add(this);
+    }
+    private void OnMouseDown()
+    {
+        if(cellParams.IsEmpty)
+        {
+            Instantiate(hexaInfo.hexaPrefab, transform.position, transform.rotation);
+            cellParams.IsEmpty = false;
+            hexaInfo.cellsOnScene.Add(this);
+        }
+    }
+}
+[System.Serializable]
+public class CellParams
+{
+    public bool IsEmpty = true;
+}
---
+++ Hive/Assets/Scripts/SOHexaInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Hexa Info", menuName = "Custom/HexaInfo")]
+public class SOHexaInfo : ScriptableObject
+{
+    public GameObject hexaPrefab;
+
+    public List<HexaCell> cellsOnScene;
+}
